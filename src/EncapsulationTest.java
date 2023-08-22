5c5d63ccc402285ee99bb7b676bab25a5eca36a7
masuma90 <masumasufian@gmail.com>
2023-08-21 23:11:20 -0400

    JavaOOProject

---
+++ src/EncapsulationTest.java
@@ -0,0 +1,9 @@
+public class EncapsulationTest {
+    public static void main(String[] args) {
+        Encapsulation encapsulation= new Encapsulation();
+        encapsulation.setId(12);
+        System.out.println(encapsulation.getId());
+    }
+
+    }
+
